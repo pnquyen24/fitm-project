e64bda9f6876da4c4cf9d8b203f73c0a6c6a4612
Arthur-Cap <thanhdat.ctd2003@gmail.com>
2023-09-21 07:39:49 +0700

    Update Readme FE

--- FITM_FE/src/Components/Static/Content/Page.js
+++ FITM_FE/src/Components/Static/Content/Page.js
@@ -1,14 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import "./Page.css";
-import Header from '../Header/header';
+import Header from "../Header/header";
+import { click } from "@testing-library/user-event/dist/click";
 
+function Page({ isOpen, setOpen }) {
 
-function Page ({isOpen, setOpen}){
-    return (
-        <div className='Page'> 
-            <Header isOpen={isOpen} setOpen={setOpen}></Header>
-            
-        </div>
-    )
+  let [input, setInput] = useState("");
+  let [noti, setNoti] = useState("");
+  let [Click, setClick] = useState("");
+  useEffect(() => {
+    console.log("here");
+    if (input.length < 8) {
+      setNoti("chưa hợp lệ");
+    } else setNoti("hợp lệ");
+  }, [input]);
+
+  return (
+    <div className="Page">
+      <Header isOpen={isOpen} setOpen={setOpen}></Header>
+
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></input>
+      <span> {" "} {input} là {noti} </span>
+      <button disabled={noti === "chưa hợp lệ" ? true : false}> {" "} {noti === "chưa hợp lệ" ? "Không cho bấm" : "Cho bấm"} {" "}
+      </button>
+    </div>
+  );
 }
 export default Page;
