eab75a71fea94c4427ebf38ad6dea460b4e78df3
Bùi Ngọc Bảo Trân <138299083+2bngbtran0@users.noreply.github.com>
2023-09-22 17:12:43 +0700

    BE_ViewInfo

---
+++ FITM_BE/Controllers/ViewPersonalInfoController.cs
@@ -0,0 +1,34 @@
+﻿using FITM_BE.Entity;
+using FITM_BE.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FITM_BE.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MembersController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public MembersController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{username}")]
+        public async Task<ActionResult<Member>> GetMember(string username)
+        {
+            var member = await _context.Members.FirstOrDefaultAsync(m => m.Username == username);
+
+            if (member == null)
+            {
+                return NotFound();
+            }
+
+            return member;
+        }
+    }
+}
--- FITM_FE/src/Components/Static/Profile/Profile.js
+++ FITM_FE/src/Components/Static/Profile/Profile.js
@@ -1,4 +1,61 @@
-function Profile(){
-    return <h1> Profile </h1>
-  }
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import "./Profile.css";
+
+function Profile(item){
+  
+  const [data, setData] = useState([]);
+  const [username, setUsername] = useState(item.username);
+
+  useEffect(() => {
+    axios.get('https://localhost:7226/Members/${username}')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, [username]);
+
+
+  return (
+    <div>
+      <table id='user'>
+        
+          <tr>
+            <th>Full Name</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Password</th>
+            <th>Student ID</th>
+            <th>DOB</th>
+            <th>Phone Number</th>
+            <th>Bank Name</th>
+            <th>Bank Number</th>
+            <th>Avatar</th>
+            <th>Status</th>
+          </tr>
+        
+        
+          {data.map(item => (
+            <tr key={item.id}>
+              <td>{item.fullName}</td>
+              <td>{item.username}</td>
+              <td>{item.email}</td>
+              <td>{item.password}</td>
+              <td>{item.studentID}</td>
+              <td>{item.dob}</td>
+              <td>{item.phoneNumber}</td>
+              <td>{item.bankName}</td>
+              <td>{item.bankNumber}</td>
+              <td>{item.avatar}</td>
+              <td>{item.status}</td>
+            </tr>
+          ))}
+        
+      </table>
+    </div>
+  );
+}
+
 export default Profile;
