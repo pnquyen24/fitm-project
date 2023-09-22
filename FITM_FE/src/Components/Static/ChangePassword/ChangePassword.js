7e6cac572e8bdbbad4c75193c53680109bd6b20c
Arthur-Cap <thanhdat.ctd2003@gmail.com>
2023-09-22 20:37:42 +0700

    Commit

---
+++ FITM_FE/src/Components/Static/ChangePassword/ChangePassword.js
@@ -0,0 +1,10 @@
+function ChangePassword() {
+  return (
+    <div className="ChangePassword">
+      <h1>Enter here</h1>
+
+      <input type="text"></input>
+    </div>
+  );
+}
+export default ChangePassword;
