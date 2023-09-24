df52052b388446e100a3f94850fac311ace46f9d
Bùi Ngọc Bảo Trân <138299083+2bngbtran0@users.noreply.github.com>
2023-09-24 21:38:58 +0700

    changed: 21:38

--- FITM_FE/src/Components/Static/Profile/Profile.js
+++ FITM_FE/src/Components/Static/Profile/Profile.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import axios from 'axios';
+import './Profile.css';
 
 function Profile({memberId}) {
   const [member, setMember] = useState(null);
 
   useEffect(() => {
-    axios.get(`https://localhost:7226/apis/Member/Get/${memberId}`)
+    axios.get(`https://localhost:7226/apis/Member/Get`)
       .then(response => {
         setMember(response.data);
       })
@@ -20,9 +21,52 @@ function Profile({memberId}) {
 
   return (
     <div>
-      <h2>{member.name}</h2>
-      <p>Email: {member.email}</p>
-      <p>Phone: {member.phone}</p>
+      
+      <h1><span className='wellcome_name'>{member.fullName}</span>'s Information</h1>
+<table>
+    <tr>
+    <th>Avatar</th>
+    <td>{member.avatar}</td>
+  </tr>
+  <tr>
+    <th>Full Name</th>
+    <td>{member.fullName}</td>
+  </tr>
+  <tr>
+    <th>Username</th>
+    <td>{member.username}</td>
+  </tr>
+  <tr>
+    <th>Email</th>
+    <td>{member.email}</td>
+  </tr> 
+  <tr>
+    <th>Student ID</th>
+    <td>{member.studentID}</td>
+  </tr>
+  <tr>
+    <th>Birth date</th>
+    <td>{member.dob}</td>
+  </tr>
+  <tr>
+    <th>Phone Number</th>
+    <td>{member.phoneNumber}</td>
+  </tr>
+  <tr>
+    <th>Bank Name</th>
+    <td>{member.bankName}</td>
+  </tr>
+  <tr>
+    <th>Bank Number</th>
+    <td>{member.bankNumber}</td>
+  </tr>
+</table>
+
+<div>
+  <button><a href='#'>Change Information</a></button>
+</div>
+
+
     </div>
   );
 }
