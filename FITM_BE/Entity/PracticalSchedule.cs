f2121ed2eac791db13d5e3d8cdb36f8aea41520a
Ngô Dương <ngduong0710@gmail.com>
2023-09-30 21:08:01 +0700

    Create Practical Schedule entity

---
+++ FITM_BE/Entity/PracticalSchedule.cs
@@ -0,0 +1,37 @@
+﻿using FITM_BE.Entity.Core;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace FITM_BE.Entity
+{
+    [Index(nameof(StartDate))]
+    public class PracticalSchedule : Entity<int>
+    {
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [StringLength(200)]
+        public string? Description { get; set; }
+        
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime StartDate { get; set; }
+        
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        [StringLength(3)]
+        public int Room { get; set; }
+
+        [Required]
+        [StringLength(7)]
+        public string BackgroundColor { get; set; }
+
+        [Required]
+        [StringLength(7)]
+        public string TextColor { get; set; }
+    }
+}
