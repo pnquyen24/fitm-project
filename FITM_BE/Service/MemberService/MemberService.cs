e66dc3ab9daa870a758730a3e925a51c5dab6ad9
Bùi Ngọc Bảo Trân <138299083+2bngbtran0@users.noreply.github.com>
2023-10-01 09:09:44 +0700

    Changed: 9h09, 1/10

--- FITM_BE/Service/MemberService/MemberService.cs
+++ FITM_BE/Service/MemberService/MemberService.cs
@@ -1,22 +1,35 @@
 ﻿using AutoMapper;
 using FITM_BE.Authentication;
 using FITM_BE.Entity;
+using FITM_BE.Service.EmailService;
 using FITM_BE.Service.MemberService.Dtos;
 using FITM_BE.Util;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace FITM_BE.Service.MemberService
 {
     public class MemberService : ServiceBase, IMemberService
     {
         private readonly IAccountService _accountService;
+        private readonly IEmailSender _emailSender;
+        private readonly IConfiguration _configuration;
 
-        public MemberService(IRepository repository, IMapper mapper, IAccountService accountService) : base(repository, mapper)
+        public MemberService(IRepository repository, IMapper mapper, IAccountService accountService, IEmailSender emailSender, IConfiguration configuration) : base(repository, mapper)
         {
             _accountService = accountService;
+            _emailSender = emailSender;
+            _configuration = configuration;
         }
 
         public async Task<MemberGeneratedDto> Create(CreateMemberDto createMemberDto)
         {
+            var existingMember = await CheckExistEmail(createMemberDto.Email);
+            if (existingMember != null)
+            {
+                throw new Exception("Email đã tồn tại");
+            }
+
             var newMember = _mapper.Map<Member>(createMemberDto);
             _accountService.GenerateAccount(ref newMember, out string newPassword);
 
@@ -25,7 +38,35 @@ namespace FITM_BE.Service.MemberService
             var member = await _repository.Add(newMember);
             member.Password = newPassword;
 
+            // Gửi email thông báo tài khoản mới
+            await SendNewAccountEmail(member.Email, member.Username, member.Password);
+
             return _mapper.Map<MemberGeneratedDto>(member);
+        }
+        private async Task SendNewAccountEmail(string email, string username, string password)
+        {
+            var message = new Message
+            (
+                new string[]
+                {
+            email
+                },
+                "New Account",
+                "<p>This is your new account:</p>" +
+                "<ul>" +
+                "<li>Username: " + username + "</li>" +
+                "<li>Password: " + password + "</li>" +
+                "</ul>"
+            );
+            await _emailSender.SendEmailAsync(message);
+        }
+
+        private async Task<Member?> CheckExistEmail(string email)
+        {
+            Member? member = await _repository
+                                .GetAll<Member>()
+                                .FirstOrDefaultAsync(m => m.Email.Equals(email));
+            return member;
         }
 
         public IEnumerable<MemberGeneratedDto> GetAll()
---
+++ FITM_FE/src/Components/HR/CreateMember.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function CreateMember() {
+    return(<h1>Hello</h1>)
+}
+
+export default CreateMember;
