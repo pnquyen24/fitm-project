46884a8acd9923266c40cce575de48746dcf816e
Ngô Dương <ngduong0710@gmail.com>
2023-10-01 21:33:14 +0700

    Show practical schedules on calendar

--- FITM_FE/src/Components/SpecialisedManager/PracticalSchedule.js
+++ FITM_FE/src/Components/SpecialisedManager/PracticalSchedule.js
@@ -1,28 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./PracticalSchedule.css";
-import { Container, Paper } from "@mui/material";
+import { Box } from "@mui/material";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import listPlugin from "@fullcalendar/list";
 import interactionPlugin from "@fullcalendar/interaction";
 
+const baseURL = "https://localhost:7226/apis/PracticalSchedule/ViewPracticalSchedules";
+
 function PracticalSchedule() {
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        axios.defaults.headers["Authorization"] = `Bearer ${localStorage.getItem("token")}`;
+        axios.get(baseURL)
+            .then((response) => {
+                const events = response.data.map(item => {
+                    return {
+                        title: item.title,
+                        start: item.startDate,
+                        end: item.endDate
+                    }
+                });
+                setEvents(events);
+            })
+            .catch((error) => {
+                // error
+                console.log(error.message);
+            });
+    }, []);
+
     return (
-        <div>
-            <Container>
-                <Paper elevation={3}>
-                    <FullCalendar
-                        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
-                        headerToolbar={{
-                            left: "prev,next today",
-                            center: "title",
-                            right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth"
-                        }}
-                        initialView="dayGridMonth"
-                    />
-                </Paper>
-            </Container>
+        <div id="calendar">
+            <Box>
+                <FullCalendar
+                    editable
+                    events={events}
+                    headerToolbar={{
+                        left: "prev,next today",
+                        center: "title",
+                        right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth"
+                    }}
+                    initialView="dayGridMonth"
+                    plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
+                    selectable
+                />
+            </Box>
         </div>
     );
 }
