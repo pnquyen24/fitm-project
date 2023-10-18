26650c0375d6ff935c7159d143172a53d66e3262
QuyenPN <pnquyen.nvla2.2019@gmail.com>
2023-10-18 21:33:33 +0700

    update data

--- FITM_FE/src/Variable/Redux/store.js
+++ FITM_FE/src/Variable/Redux/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import scheduleSlice from "../../Components/SpecialisedManager/scheduleSlice";
+import attendanceSlice from "./Slice/attendanceSlice";
+import colorSlice from "./Slice/colorSlice";
 
 const store = configureStore({
     reducer: {
