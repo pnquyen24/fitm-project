bfbccc975ceaa23dfe70c1f1200c27b1a1e50f5f
Ngô Dương <ngduong0710@gmail.com>
2023-10-18 14:20:02 +0700

    Remove unuse library

--- FITM_FE/src/Components/SpecialisedManager/AttendancePractical.js
+++ FITM_FE/src/Components/SpecialisedManager/AttendancePractical.js
@@ -1,12 +1,5 @@
 import { useEffect } from "react";
-import { Paper, Radio, RadioGroup } from "@mui/material";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import CustomeFCLabel from "../Member/Label/CustomeFCLabel";
+import { Paper } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import {
     fetchList,
