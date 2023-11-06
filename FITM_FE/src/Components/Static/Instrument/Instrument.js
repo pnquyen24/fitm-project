f2cbbf4fa9c3cd1ad04459f44801b8d7c8481a5e
Ngô Dương <ngduong0710@gmail.com>
2023-11-06 15:46:49 +0700

    update instrument feature

--- FITM_FE/src/Components/Static/Instrument/Instrument.js
+++ FITM_FE/src/Components/Static/Instrument/Instrument.js
@@ -1,12 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
+    deleteInstruments,
     getAllInstruments,
     getInstruments,
+    toggleModalInstrument,
 } from "../../../Variable/Redux/Slice/instrumentSlice";
-import { IconButton, Paper, Stack } from "@mui/material";
+import {
+    Button,
+    Card,
+    CardHeader,
+    IconButton,
+    Stack,
+    Typography,
+} from "@mui/material";
 import PaginationTable from "../../Member/Table/PaginationTable/PaginationTable";
+import CustomeAlert from "../../Member/Alert/CustomeAlert";
 import { Delete, Edit } from "@mui/icons-material";
+import ModalInstrument from "./ModalInstrument";
 
 function Instrument() {
     document.title = "Instrument";
@@ -16,6 +27,9 @@ function Instrument() {
     const dispatch = useDispatch();
     const instruments = useSelector(getInstruments);
 
+    const [instrumentInfo, setInstrumentInfo] = useState();
+    const [isEdit, setIsEdit] = useState();
+
     useEffect(() => {
         dispatch(getAllInstruments());
     }, [dispatch]);
@@ -24,6 +38,7 @@ function Instrument() {
         { id: "index", label: "#" },
         { id: "name", label: "Name" },
         { id: "count", label: "Count" },
+        { id: "items", label: "Item Ids" },
         { id: "action", label: "Action" },
     ];
 
@@ -40,10 +55,10 @@ function Instrument() {
             count,
             action: (
                 <Stack direction="row" spacing={2}>
-                    <IconButton>
+                    <IconButton onClick={handleUpdate(id)}>
                         <Edit color="primary" />
                     </IconButton>
-                    <IconButton>
+                    <IconButton onClick={handleDelete(id)}>
                         <Delete color="error" />
                     </IconButton>
                 </Stack>
@@ -51,11 +66,45 @@ function Instrument() {
         };
     }
 
+    function handleCreate() {
+        setIsEdit(false);
+        dispatch(toggleModalInstrument(true));
+    }
+
+    function handleUpdate(instrument) {
+        setInstrumentInfo(instrument);
+        setIsEdit(true);
+        dispatch(toggleModalInstrument(true));
+    }
+
+    function handleDelete(instrumentId) {
+        CustomeAlert.confirm("Are you sure delete it?", "Delete", "No").then(
+            (result) => {
+                if (result.isConfirmed) {
+                    dispatch(deleteInstruments(instrumentId));
+                }
+            }
+        );
+    }
+
     return (
         <>
-            <Paper sx={{ width: "96%", marginTop: 3, overflow: "hidden" }}>
+            <ModalInstrument instrumentInfo={instrumentInfo} isEdit={isEdit} />
+            <Card sx={{ width: "96%", marginTop: 3, overflow: "hidden" }}>
+                <CardHeader
+                    title={
+                        <Typography component={"span"} variant="subtitle1">
+                            Instrument Table
+                        </Typography>
+                    }
+                    action={
+                        <Button variant="contained" onClick={handleCreate}>
+                            Create
+                        </Button>
+                    }
+                />
                 <PaginationTable rows={rows} columns={columns} />
-            </Paper>
+            </Card>
         </>
     );
 }
