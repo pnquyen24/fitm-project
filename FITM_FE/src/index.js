09a821c572de5dddcd13f534929cc81b4a4a1e66
Ngô Dương <ngduong0710@gmail.com>
2023-11-11 01:55:56 +0700

    fix api

--- FITM_FE/src/index.js
+++ FITM_FE/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import axiosClient from "./Variable/Api/api";
+import { axiosLogin } from "./Variable/Api/api";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import ForgotPassword from "./Components/Authentication/ForgotPassword/ForgotPassword";
@@ -47,7 +47,7 @@ checkLoginStatus();
 async function checkLoginStatus() {
     if (localStorage.getItem("token") !== null) {
         try {
-            const response = await axiosClient.get(CHECK_LOGIN_URL);
+            const response = await axiosLogin.get(CHECK_LOGIN_URL);
             Authented = true;
             return response.data;
         } catch (error) {
